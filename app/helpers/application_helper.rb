545621769b7898fa76fc3c408d2d69e85d7e645b
root_suke <rootsuke@gmail.com>
2019-01-15 23:29:14 +0900

    create full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title = "")
+    base_title = "Price Manager"
+    return base_title if page_title.empty?
+
+    "#{page_title} | #{base_title}"
+  end
 end
