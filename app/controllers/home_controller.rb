70ffe8b0ce02a87b522d295fed19a34f6911fa6a
root_suke <rootsuke@gmail.com>
2019-01-16 23:45:17 +0900

    add tentative crawl function

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    if params[:url].present?
+      crawler = Crawler::CrawlerService.new(url: params[:url], site_type: params[:site][:name])
+      @crawl_result = crawler.crawl
+    end
   end
 end
---
+++ app/services/crawler/crawler_service.rb
@@ -0,0 +1,30 @@
+require 'kconv'
+
+module Crawler
+  class CrawlerService
+    def initialize(url: "", site_type: "")
+      @url = url
+      @site_type = site_type
+    end
+
+    def crawl
+      result = {}
+      Anemone.crawl(@url, depth_limit: 0) do |anemone|
+        anemone.on_every_page do |page|
+          # parse by Nokogiri, after convert character_code into utf-8
+          item = Nokogiri::HTML.parse(page.body.toutf8)
+
+          # name
+          result[:name] = item.xpath("//span[@id=\"productTitle\"]").text.strip
+          # display_price
+          result[:display_price] = item.xpath("//span[@id=\"priceblock_ourprice\"]").text.strip
+          # current_price
+          result[:current_price] = item.xpath("//span[@id=\"priceblock_ourprice\"]").text.strip.delete("^0-9")
+          # image_url
+          result[:image_url] = item.xpath("//img[@id=\"landingImage\"]").attribute("data-old-hires").text.strip
+        end
+      end
+      result
+    end
+  end
+end
