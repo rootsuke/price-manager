0c924b70ef169a982c9a6fd4de73fb6607aaf28a
root_suke <rootsuke@gmail.com>
2019-01-20 02:23:55 +0900

    crawl only when both params[:product_url] and params[:site][:name] are given

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    if params[:product_url].present?
+    if params[:product_url].present? && params[:site][:name].present?
       crawler = Crawler::CrawlerService.new(product_url: params[:product_url], site_type: params[:site][:name])
       @crawl_result = crawler.crawl
     end
