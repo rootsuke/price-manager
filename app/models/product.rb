4a52586b0813f3312564185f97c2b42c7f1ed226
root_suke <rootsuke@gmail.com>
2019-01-20 02:24:13 +0900

    add validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,8 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :base_price, presence: true
   validates :display_price, presence: true
+  validates :product_url, presence: true
+  validates :site_type, presence: true
 
   def calc_price_drop
     current_price - base_price
