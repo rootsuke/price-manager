0a7df1d0bac2147c8ca8f7ee50ec82791cbf2797
root_suke <rootsuke@gmail.com>
2019-01-20 01:19:53 +0900

    fix typo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    if params[:url].present?
+    if params[:product_url].present?
       crawler = Crawler::CrawlerService.new(product_url: params[:product_url], site_type: params[:site][:name])
       @crawl_result = crawler.crawl
     end
