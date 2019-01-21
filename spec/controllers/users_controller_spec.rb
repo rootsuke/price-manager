1c8f95a08ec9f4363593e747204d7637823df7af
root_suke <rootsuke@gmail.com>
2019-01-21 23:44:32 +0900

    create users_controller_spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:user) { create(:user) }
+  let(:oldest_product) { create(:product, created_at: 2.days.ago,   user: user) }
+  let(:old_product)    { create(:product, created_at: 1.day.ago,    user: user) }
+  let(:new_product)    { create(:product, created_at: DateTime.now, user: user) }
 
   describe "GET #show" do
+    login_user
+
+    before do
+      get :show, params: { id: user.id }
+    end
+
     it "returns http success" do
-      get :show
       expect(response).to have_http_status(:success)
     end
-  end
 
+    it "render show page" do
+      expect(response).to render_template(:show)
+    end
+
+    it "assigns @user" do
+      expect(assigns(:user)).to eq user
+    end
+
+    it "assigns @posts" do
+      expect(assigns(:products)).to eq [oldest_product, old_product, new_product]
+    end
+  end
 end
