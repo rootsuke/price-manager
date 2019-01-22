978ac651f23fdad1d2183a84b77d4b3882485cad
root_suke <rootsuke@gmail.com>
2019-01-22 22:10:46 +0900

    create product_model spec

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
     base_price { 9999 }
     current_price { 9000 }
     display_price { "¥9,000" }
-    product_url { "#" }
-    image_url { "#" }
+    product_url { "http://sample_product.example.com" }
+    image_url { "http://sample_image.example.com" }
     site_type { "amazon" }
     user
   end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:product) { create(:product) }
+
+  it "is invalid without name" do
+    product.name = nil
+    product.valid?
+    expect(product.errors[:name]).to include "can't be blank"
+  end
+
+  it "is invalid without base_price" do
+    product.base_price = nil
+    product.valid?
+    expect(product.errors[:base_price]).to include "can't be blank"
+  end
+
+  it "is invalid without current_price" do
+    product.current_price = nil
+    product.valid?
+    expect(product.errors[:current_price]).to include "can't be blank"
+  end
+
+  it "is invalid without display_price" do
+    product.display_price = nil
+    product.valid?
+    expect(product.errors[:display_price]).to include "can't be blank"
+  end
+
+  it "is invalid without product_url" do
+    product.product_url = nil
+    product.valid?
+    expect(product.errors[:product_url]).to include "can't be blank"
+  end
+
+  it "is invalid without site_type" do
+    product.site_type = nil
+    product.valid?
+    expect(product.errors[:site_type]).to include "can't be blank"
+  end
 end
