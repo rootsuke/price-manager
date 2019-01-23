156bd173ee2731dcecf4975774cbad5beacbc08a
root_suke <rootsuke@gmail.com>
2019-01-23 23:59:08 +0900

    remove comment_out

--- app/services/crawler/update_price_service.rb
+++ app/services/crawler/update_price_service.rb
@@ -20,22 +20,6 @@ module Crawler
       result
     end
 
-    # def update_product_price
-    #   result = {}
-    #   Anemone.crawl(@product_url, depth_limit: 0) do |anemone|
-    #     anemone.on_every_page do |page|
-    #       # parse by Nokogiri, after convert character_code into utf-8
-    #       item = Nokogiri::HTML.parse(page.body.toutf8)
-
-    #       # display_price
-    #       result[:display_price] = item.xpath("//span[@id=\"priceblock_ourprice\"]").text.strip
-    #       # current_price
-    #       result[:current_price] = result[:display_price].delete("^0-9")
-    #     end
-    #   end
-    #   result
-    # end
-
     private
 
       def crawling_price_on_amazon(item, result)
