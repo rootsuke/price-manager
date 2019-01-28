ae6b872640a471ad638a5876b97dd7bc8f30235b
root_suke <rootsuke@gmail.com>
2019-01-29 02:24:16 +0900

    display 10 products per a page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
     @user = User.find(params[:id])
-    @products = @user.products.paginate(page: params[:page])
+    @products = @user.products.paginate(page: params[:page], per_page: 10)
   end
 end
