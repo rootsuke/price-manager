f6864cae79a4d59148271e1e2f5830de1926cfc0
root_suke <rootsuke@gmail.com>
2019-04-07 23:35:59 +0900

    omniauth-facebook settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get "/help", to: "home#help"
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
   resources  :users,    only: :show
   resources  :products, only: [:create, :show, :update, :destroy]
   post "/update_all_products", to: "products#update_all"
