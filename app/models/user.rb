880d73b323a47fda458154e540f9f59835d7a00a
root_suke <rootsuke@gmail.com>
2019-04-09 00:53:25 +0900

    enable signup and login with twitter account

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  devise :omniauthable, omniauth_providers: %i(facebook)
+  devise :omniauthable, omniauth_providers: %i(facebook twitter)
 
   has_many :products, dependent: :destroy
 
