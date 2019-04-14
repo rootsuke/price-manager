489e0d16288af126b8139191c9ced0394a0ad763
root_suke <rootsuke@gmail.com>
2019-04-14 21:41:41 +0900

    add task to update all user's products price
    by heroku scheduler, this task run at 4:00 AM on everyday

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,16 @@ class User < ApplicationRecord
       # user.skip_confirmation!
     end
   end
+
+  def update_all_product
+    products.each do |product|
+      crawler = Crawler::UpdatePriceService.new(product_url: product.product_url, site_type: product.site_type)
+      prices = crawler.update_product_price
+      product.update_attributes(prices)
+      unless product.save
+        puts "#{name} : #{product.name}更新失敗"
+        next
+      end
+    end
+  end
 end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -2,3 +2,11 @@ desc "This task is called by the Heroku scheduler add-on"
 task :test_task => :environment do
   puts "done test."
 end
+
+task :update_all => :environment do
+  users = User.all
+  users.each do |user|
+    user.update_all_product
+  end
+  puts "done updating price."
+end
