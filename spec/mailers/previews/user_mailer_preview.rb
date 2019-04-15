733833dc2a4019682c2700e9dfb80ce43bbbae13
root_suke <rootsuke@gmail.com>
2019-04-15 23:41:17 +0900

    create UserMailer

---
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/notify_price
+  def notify_price
+    UserMailerMailer.notify_price
+  end
+
+end
