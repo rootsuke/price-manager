1579e136a3568add0175d7b07ad0149ce83d93a4
root_suke <rootsuke@gmail.com>
2019-04-16 03:00:38 +0900

    action mailer preview settings

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,9 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/notify_price
   def notify_price
-    UserMailerMailer.notify_price
+    user = User.first
+    UserMailer.notify_price(user)
   end
-
 end
