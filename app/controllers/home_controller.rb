fcbd62f81222548d12d9eb2ed509c76dedea690a
root_suke <rootsuke@gmail.com>
2019-06-28 17:45:25 +0900

    get product informaiton by ajax

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,18 @@
 class HomeController < ApplicationController
   def index
-    if params[:product_url].present? && params[:site][:name].present?
-      crawler = Crawler::CrawlerService.new(product_url: params[:product_url], site_type: params[:site][:name])
-      @crawl_result = crawler.crawl
-    end
   end
 
   def help
+  end
+
+  def crawl
+    if params[:product_url].present? && params[:site][:name].present?
+      crawler = Crawler::CrawlerService.new(product_url: params[:product_url], site_type: params[:site][:name])
+      @crawl_result = crawler.crawl
+      respond_to do |format|
+        format.html { redirect_to root_url }
+        format.js
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get "/help", to: "home#help"
+  get "/home/crawl", to: "home#crawl"
   devise_for :users, controllers: { registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
   resources  :users,    only: :show
   resources  :products, only: [:create, :show, :update, :destroy]
