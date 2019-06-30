8c076c554b338b1a10eac653f6e5f975b0f0fd31
root_suke <rootsuke@gmail.com>
2019-06-30 23:02:30 +0900

    use activejob when run price_updating tasks

--- app/jobs/update_all_product_price_job.rb
+++ app/jobs/update_all_product_price_job.rb
@@ -3,12 +3,13 @@ class UpdateAllProductPriceJob < ApplicationJob
 
   def perform(user)
     products = user.products.all
+    return if products.empty?
     products.each do |product|
       crawler = Crawler::UpdatePriceService.new(product_url: product.product_url, site_type: product.site_type)
       prices = crawler.update_product_price
       product.update_attributes(prices)
       unless product.save
-        return
+        next
       end
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,14 +22,6 @@ class User < ApplicationRecord
   end
 
   def update_all_product
-    products.each do |product|
-      crawler = Crawler::UpdatePriceService.new(product_url: product.product_url, site_type: product.site_type)
-      prices = crawler.update_product_price
-      product.update_attributes(prices)
-      unless product.save
-        puts "#{name} : #{product.name}更新失敗"
-        next
-      end
-    end
+    UpdateAllProductPriceJob.perform_later(self)
   end
 end
