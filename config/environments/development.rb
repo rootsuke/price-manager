f49208fa4215d1b36c1b1c1423193b0dcb8d9339
root_suke <rootsuke@gmail.com>
2019-07-01 18:03:09 +0900

    mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,12 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # mailer
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
   host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.perform_caching = false
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
