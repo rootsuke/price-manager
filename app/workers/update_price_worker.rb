57aa71dffd0e7a17e961736d1cffa81d1fa80750
root_suke <rootsuke@gmail.com>
2019-07-03 00:49:15 +0900

    fix worker

--- app/workers/update_price_worker.rb
+++ app/workers/update_price_worker.rb
@@ -8,8 +8,7 @@ class UpdatePriceWorker
     products.each do |product|
       crawler = Crawler::UpdatePriceService.new(product_url: product.product_url, site_type: product.site_type)
       prices = crawler.update_product_price
-      product.update_attributes(prices)
-      next unless product.save
+      next unless product.update_attributes(prices)
     end
     UserMailer.notify_price(user).deliver_now if mailer
   end
